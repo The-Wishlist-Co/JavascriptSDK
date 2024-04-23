e9948a649a685f7c189ca14cf10285906e165a19
mjhampshire <38122549+mjhampshire@users.noreply.github.com>
2024-04-23 10:21:16 +1000

    Update addCustomerAddress.js
    This API is depreciated

--- customerService/addCustomerAddress.js
+++ customerService/addCustomerAddress.js
@@ -1,20 +1,19 @@
 // /customerservice/api/v2/customers/f98e5013-6361-4540-8ffc-d73ba27d1e44/address
+// This API has been depreciated
 
-const axiosInstance=require('../axios/AxiosInstance');
-
-function addCustomerAddress(obj) {
-axiosInstance.post(`/customerservice/api/v2/customers/${obj.customerID}/address`, obj.newAddress,
-{headers: {
-  'Content-Type': 'application/json', 
-  'Authorization':  `Bearer ${obj.token}`,
-  'X-TWC-Tenant': obj.tenant
-}})
-  .then(response => {
-    obj.onSuccess(response.data);
-  })
-  .catch(error => {
-    obj.onError(error?.response||error);
-  });
-}
-
-module.exports=addCustomerAddress;
+// const axiosInstance=require('../axios/AxiosInstance');
+// function addCustomerAddress(obj) {
+// axiosInstance.post(`/customerservice/api/v2/customers/${obj.customerID}/address`, obj.newAddress,
+// {headers: {
+//  'Content-Type': 'application/json', 
+//  'Authorization':  `Bearer ${obj.token}`,
+//  'X-TWC-Tenant': obj.tenant
+// }})
+//  .then(response => {
+//    obj.onSuccess(response.data);
+//  })
+//  .catch(error => {
+//    obj.onError(error?.response||error);
+//  });
+// }
+// module.exports=addCustomerAddress;
