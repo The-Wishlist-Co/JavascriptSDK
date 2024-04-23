b617c2fb6ff4115735a6ee360f0296a74dac9493
mjhampshire <38122549+mjhampshire@users.noreply.github.com>
2024-04-23 10:12:07 +1000

    Update validateCustomer.js
    Depreciated this API

--- customerService/validateCustomer.js
+++ customerService/validateCustomer.js
@@ -1,20 +1,21 @@
+// This API has been depreciated.
+//
+//const axiosInstance=require('../axios/AxiosInstance');
 
+//function validateCustomer(obj) {
+//axiosInstance.post(`/customerservice/api/v2/customers/validate`, obj.createCustomerBody,
+//{headers: {
+//  'Content-Type': 'application/json', 
+//  'Authorization':  `Bearer ${obj.token}`,
+//  'X-TWC-Tenant': obj.tenant
+//}})
+//  .then(response => {
+//    obj.onSuccess(response.data);
+//  })
+//  .catch(error => {
+//    obj.onError(error?.response||error);
+//  });
+//}
 
-const axiosInstance=require('../axios/AxiosInstance');
+// module.exports=validateCustomer;
 
-function validateCustomer(obj) {
-axiosInstance.post(`/customerservice/api/v2/customers/validate`, obj.createCustomerBody,
-{headers: {
-  'Content-Type': 'application/json', 
-  'Authorization':  `Bearer ${obj.token}`,
-  'X-TWC-Tenant': obj.tenant
-}})
-  .then(response => {
-    obj.onSuccess(response.data);
-  })
-  .catch(error => {
-    obj.onError(error?.response||error);
-  });
-}
-
-module.exports=validateCustomer;
